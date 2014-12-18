19b0c48b1172a88d0feed2edb5f4f132a0d2420e
Sebastien Maloron <contact@smaloron.eu>
2014-12-18 22:31:21 +0100

    Ajout de couleurs

--- src/fr/m2i/appjournal2014/Login.java
+++ src/fr/m2i/appjournal2014/Login.java
@@ -4,12 +4,27 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class Login extends Activity {
 
+	private EditText editTextIdentifiant;
+	private EditText editTextMotDePasse;
+	private Button btOk;
+	private Button btAnnuler;
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.login);
+		
+		//Récupération des contrôles de l'activité
+		btOk = (Button) findViewById(R.id.buttonOK);
+		btAnnuler = (Button) findViewById(R.id.buttonAnnuler);
+		editTextIdentifiant = (EditText) findViewById(R.id.editTextIdentifiant);
+		editTextMotDePasse = (EditText) findViewById(R.id.editTextMotDePasse);
+		
 	}
 }
