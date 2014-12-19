9dde228de33856cd28af57ff8d1fea7de1d11610
Sebastien Maloron <contact@smaloron.eu>
2014-12-19 19:38:51 +0100

    Corrections sur la listView journalistes

--- src/fr/m2i/journal2014/Journalistes.java
+++ src/fr/m2i/journal2014/Journalistes.java
@@ -66,35 +66,35 @@ public class Journalistes extends ListActivity {
 		record.put("nom", "Edwy Plenel");
 		record.put("statut", "rédacteur en chef");
 		record.put("id", "1");
-		record.put("image", "plenel.png");
+		//record.put("image", "plenel.png");
 		listJournalistes.add(record);
 
 		record = new HashMap<String, String>();
 		record.put("nom", "Fabrice Arfi");
 		record.put("statut", "journaliste");
 		record.put("id", "2");
-		record.put("image", "arfi.png");
+		//record.put("image", "arfi.png");
 		listJournalistes.add(record);
 
 		record = new HashMap<String, String>();
 		record.put("nom", "Fabrice Lhomme");
 		record.put("statut", "journaliste");
 		record.put("id", "3");
-		record.put("image", "lhomme.png");
+		//record.put("image", "lhomme.png");
 		listJournalistes.add(record);
 
 		record = new HashMap<String, String>();
 		record.put("nom", "Gérard Davet");
 		record.put("statut", "journaliste");
 		record.put("id", "4");
-		record.put("image", "davet.png");
+		//record.put("image", "davet.png");
 		listJournalistes.add(record);
 
 		record = new HashMap<String, String>();
 		record.put("nom", "Pierre Haski");
 		record.put("statut", "rédacteur en chef");
 		record.put("id", "4");
-		record.put("image", "haski.png");
+		//record.put("image", "haski.png");
 		listJournalistes.add(record);
 	}
 	
