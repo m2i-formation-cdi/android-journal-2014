dfd1fdc5e8ab911ef7b25667288ac3aa36668400
Sebastien Maloron <contact@smaloron.eu>
2014-12-20 17:27:40 +0100

    Correction de l'affichage de la liste des journalistes

---
+++ src/fr/m2i/journal2014/JournalisteForm.java
@@ -0,0 +1,17 @@
+package fr.m2i.journal2014;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class JournalisteForm extends Activity {
+
+	
+	
+	@Override
+	protected void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.journaliste_form);
+	}
+}
--- src/fr/m2i/journal2014/Journalistes.java
+++ src/fr/m2i/journal2014/Journalistes.java
@@ -28,7 +28,7 @@ public class Journalistes extends ListActivity {
 		setContentView(R.layout.journalistes);
 
 		//listeViewJournalistes = (ListView) findViewById(R.id.listViewJournalistes);
-
+		RemplirListView();
 		
 	}
 
@@ -38,15 +38,15 @@ public class Journalistes extends ListActivity {
 	private void RemplirListView() {
 
 		try {
-			listeViewJournalistes = this.getListView();
+			this.listeViewJournalistes = this.getListView();
 			this.setData();
 			SimpleAdapter sa = new SimpleAdapter(this.getBaseContext(),
-					listJournalistes, R.id.LayoutListeJournalistes,
+					this.listJournalistes, R.layout.liste_journalistes,
 					new String[] { "nom", "statut", "id" }, new int[] {
 							R.id.lstJournalisteNom, R.id.lstJournalisteStatut,
 							R.id.lstJournalisteId });
 
-			listeViewJournalistes.setAdapter(sa);
+			this.listeViewJournalistes.setAdapter(sa);
 			
 		} catch (Exception e) {
 			Log.i("Erreur", e.getMessage());
@@ -59,7 +59,7 @@ public class Journalistes extends ListActivity {
 	 * Création de données artificielles pour remplir la liste des journalistes
 	 */
 	private void setData() {
-		listJournalistes = new ArrayList<Map<String, String>>();
+		this.listJournalistes = new ArrayList<Map<String, String>>();
 		Map<String, String> record;
 
 		record = new HashMap<String, String>();
@@ -67,35 +67,35 @@ public class Journalistes extends ListActivity {
 		record.put("statut", "rédacteur en chef");
 		record.put("id", "1");
 		//record.put("image", "plenel.png");
-		listJournalistes.add(record);
+		this.listJournalistes.add(record);
 
 		record = new HashMap<String, String>();
 		record.put("nom", "Fabrice Arfi");
 		record.put("statut", "journaliste");
 		record.put("id", "2");
 		//record.put("image", "arfi.png");
-		listJournalistes.add(record);
+		this.listJournalistes.add(record);
 
 		record = new HashMap<String, String>();
 		record.put("nom", "Fabrice Lhomme");
 		record.put("statut", "journaliste");
 		record.put("id", "3");
 		//record.put("image", "lhomme.png");
-		listJournalistes.add(record);
+		this.listJournalistes.add(record);
 
 		record = new HashMap<String, String>();
 		record.put("nom", "Gérard Davet");
 		record.put("statut", "journaliste");
 		record.put("id", "4");
 		//record.put("image", "davet.png");
-		listJournalistes.add(record);
+		this.listJournalistes.add(record);
 
 		record = new HashMap<String, String>();
 		record.put("nom", "Pierre Haski");
 		record.put("statut", "rédacteur en chef");
 		record.put("id", "4");
 		//record.put("image", "haski.png");
-		listJournalistes.add(record);
+		this.listJournalistes.add(record);
 	}
 	
 	public void onListItemClick(ListView parent, View v, int position, long id) {
