049fe2d4a67807a4b11506bfa6b140f731d9ece1
Sebastien Maloron <contact@smaloron.eu>
2015-01-08 16:33:18 +0100

    Ajout de la class DbConnexion

---
+++ src/fr/m2i/journal2014/models/DbConnexion.java
@@ -0,0 +1,21 @@
+package fr.m2i.journal2014.models;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
+public class DbConnexion {
+
+	private static String dbHost = "jdbc:mysql://172.26.10.32:3306/journal2014";
+	private static String dbUser = "p";
+	private static String dbPass = "b";
+
+	public static Connection connect() throws ClassNotFoundException,
+			SQLException {
+		Connection cn;
+		Class.forName("com.mysql.jdbc.Driver");
+		cn = DriverManager.getConnection(dbHost, dbUser, dbPass);
+		return cn;
+	}
+
+}
