92c052a1c1239d94860e9516413b5fe9c195eb32
Sebastien Maloron <contact@smaloron.eu>
2015-01-12 10:13:20 +0100

    résolution de l'id statut en nom de statut dans la listes des journalistes

--- src/fr/m2i/journal2014/Journalistes.java
+++ src/fr/m2i/journal2014/Journalistes.java
@@ -1,13 +1,22 @@
 package fr.m2i.journal2014;
 
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
 
+import fr.m2i.journal2014.models.DAOFichier;
+import fr.m2i.journal2014.models.DbConnexion;
+import fr.m2i.journal2014.models.GenericDAO;
+import fr.m2i.journal2014.models.PojoJournaliste;
 import android.app.Activity;
 import android.app.ListActivity;
 import android.content.Intent;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.Menu;
@@ -42,19 +51,18 @@ public class Journalistes extends ListActivity implements OnClickListener {
 		//Définition des intentions
 		intentionFormulaire = new Intent(this.getBaseContext(),JournalisteForm.class);
 		
-		//Remplissage de la liste des journalistes
-		this.RemplirListView();
-		
+		//Remplissage de la liste des journalistes depuis un tâche asynchrone
+		TacheAsynchrone asyncTask = new TacheAsynchrone();
+		asyncTask.execute("");	
 	}
 
 	/**
 	 * Remplit la liste des journalistes
 	 */
-	private void RemplirListView() {
+	private void populateListJournalistes() {
 
 		try {
 			this.listeViewJournalistes = this.getListView();
-			this.setData();
 			SimpleAdapter sa = new SimpleAdapter(this.getBaseContext(),
 					this.listJournalistes, R.layout.liste_journalistes,
 					new String[] { "nom", "statut", "id" }, new int[] {
@@ -70,48 +78,76 @@ public class Journalistes extends ListActivity implements OnClickListener {
 		}
 	}
 
-	/**
-	 * Création de données artificielles pour remplir la liste des journalistes
-	 */
-	private void setData() {
-		this.listJournalistes = new ArrayList<Map<String, String>>();
-		Map<String, String> record;
+	
+	
+	private class TacheAsynchrone extends AsyncTask <String, Integer, List<Map<String, String>>> {
 
-		record = new HashMap<String, String>();
-		record.put("nom", "Edwy Plenel");
-		record.put("statut", "rédacteur en chef");
-		record.put("id", "1");
-		//record.put("image", "plenel.png");
-		this.listJournalistes.add(record);
+		//private List<Map<String, String>> resultSet;
+		
+		@Override
+		protected List<Map<String, String>> doInBackground(String... params) {
+			List<Map<String, String>> resultSet = new ArrayList<Map<String,String>>();
+			try {
+				Connection cn = DbConnexion.connect();
+				GenericDAO dao = new GenericDAO("contributeur", cn);
+				resultSet = dao.selectAll();
+			} catch (Exception e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			} 
+			
+			return resultSet;
+		}
+		
+		/**
+		 * 
+		 */
+		protected void onPostExecute(List<Map<String, String>> rs) {
+			listJournalistes = this.getFilteredData(rs);
+			populateListJournalistes();
+			
+		} // / onPostExecute
+		
+		private List<Map<String, String>> getFilteredData(List<Map<String, String>> rs){
+			List<Map<String, String>> filteredData = new ArrayList<Map<String, String>>();
+			
+			String idStatut;
+			String statut;
+			
+			for (Map<String, String> map : rs) {
+				Map<String,String> record = new HashMap<String, String>();
+				idStatut = map.get("id_statut");
+				statut = this.getStatut(idStatut);
+				record.put("nom", map.get("prenom_contributeur") + " " + map.get("nom_contributeur"));
+				record.put("id", map.get("id_contributeur"));
+				record.put("statut", statut);
+				filteredData.add(record);	
+			}
+			
+			return filteredData;
+		}
 
-		record = new HashMap<String, String>();
-		record.put("nom", "Fabrice Arfi");
-		record.put("statut", "journaliste");
-		record.put("id", "2");
-		//record.put("image", "arfi.png");
-		this.listJournalistes.add(record);
-
-		record = new HashMap<String, String>();
-		record.put("nom", "Fabrice Lhomme");
-		record.put("statut", "journaliste");
-		record.put("id", "3");
-		//record.put("image", "lhomme.png");
-		this.listJournalistes.add(record);
-
-		record = new HashMap<String, String>();
-		record.put("nom", "Gérard Davet");
-		record.put("statut", "journaliste");
-		record.put("id", "4");
-		//record.put("image", "davet.png");
-		this.listJournalistes.add(record);
-
-		record = new HashMap<String, String>();
-		record.put("nom", "Pierre Haski");
-		record.put("statut", "rédacteur en chef");
-		record.put("id", "4");
-		//record.put("image", "haski.png");
-		this.listJournalistes.add(record);
+		private String getStatut(String idStatut){
+			String statut = "";
+			try {
+				DAOFichier dao = new DAOFichier(getBaseContext(), R.raw.statut);
+				dao.setfirstLineContainsLabel(false);
+				dao.loadData();
+				List<Map<String,String>> records = dao.findByColumn("col1", idStatut);
+				if(records.size()>0){
+					statut = records.get(0).get("col2").toString();
+				}
+			} catch (IOException e) {
+				// TODO Auto-generated catch block
+				e.printStackTrace();
+			}
+			return statut;
+			
+		}
+		
 	}
+	
+	
 	
 	public void onListItemClick(ListView parent, View v, int position, long id) {
 		String pk = listJournalistes.get(position).get("id");
