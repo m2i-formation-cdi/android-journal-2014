95c3090904212537f0d52e6722bf158c863501fd
Sebastien Maloron <contact@smaloron.eu>
2015-01-14 11:13:57 +0100

    Modif de DbConnexion pour utiliser param.propreties comme source de paramètrage

--- src/fr/m2i/journal2014/models/DbConnexion.java
+++ src/fr/m2i/journal2014/models/DbConnexion.java
@@ -3,17 +3,32 @@ package fr.m2i.journal2014.models;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
+import java.util.Properties;
+
+import android.content.Context;
+import fr.m2i.journal2014.utils.PropertyReader;
 
 public class DbConnexion {
 
+	/*
 	private static String dbHost = "jdbc:mysql://172.26.10.54:3306/journal2014";
 	private static String dbUser = "p";
 	private static String dbPass = "b";
-
-	public static Connection connect() throws ClassNotFoundException,
+*/
+	public static Connection connect(Context context) throws ClassNotFoundException,
 			SQLException {
 		Connection cn;
 		Class.forName("com.mysql.jdbc.Driver");
+		
+		PropertyReader propReader = new PropertyReader(context);
+		Properties p = propReader.getProperties("param.properties");
+		
+		String dbHost = p.getProperty("DbProtocol");
+		dbHost += p.getProperty("DbIP") + ":" + p.getProperty("DbPort");
+		dbHost += "/" + p.getProperty("BdName");
+		
+		String dbUser = p.getProperty("DbUser");
+		String dbPass = p.getProperty("DbPass");
 		cn = DriverManager.getConnection(dbHost, dbUser, dbPass);
 		cn.setAutoCommit(true);
 		return cn;
